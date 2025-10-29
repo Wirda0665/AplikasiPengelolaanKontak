8ea23f6d4f57442e15d405f0f90e082e40cff1af
Wirda0665 <wirda.hajiza2005@gmail.com>
2025-10-29 12:16:46 +0800

    Menyiapkan package MVC dan Pengaturan Database

---
+++ src/database/DatabaseConnection.java
@@ -0,0 +1,16 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class DatabaseConnection {
+    private static final String URL = "jdbc:sqlite:contacts.db";
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }    
+}
